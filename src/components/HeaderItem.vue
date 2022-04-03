599776cd0ddd4a0fc09de1221cff50a618dcbf08
Vicente <vicente.ict3@gmail.com>
2022-04-03 14:05:43 +0200

    Header component añadido

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="contenedor">
+    <h1 class="Titulo">
+        Developedia
+        <font-awesome-icon icon="earth" />    
+    </h1>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.5rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
+//Default imports
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+//Font Awesome impots
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard, faEarth } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from './App.vue'
 import router from './router'
+
+//Rendering icons
+library.add(
+    faHatWizard, 
+    faEarth
+)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component("font-awesome-icon", FontAwesomeIcon)
 
 app.mount('#app')
